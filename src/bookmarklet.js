acb44060504ea26b2dd1d9f632659173c453a284
Jan T. Sott <git@idleberg.com>
2025-09-08 11:30:03 +0200

    chore: fix linter warning

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,95 +1,94 @@
 const validOrigins = {
-  bandcamp: 'https://bandcamp.com',
-  instagram: 'https://instagram.com',
-  mixcloud: 'https://mixcloud.com',
-  soundcloud: 'https://soundcloud.com',
-  youtube: 'https://youtube.com',
+	bandcamp: 'https://bandcamp.com',
+	instagram: 'https://instagram.com',
+	mixcloud: 'https://mixcloud.com',
+	soundcloud: 'https://soundcloud.com',
+	youtube: 'https://youtube.com',
 };
 
 function getSoundcloudImage() {
-  const albumArt =  document.querySelector('.fullHero__artwork .image span');
-  const backgroundImage = albumArt.style.backgroundImage.split('"')[1];
-  const imageURL = backgroundImage.replace(/t500x500/g, 'original');
+	const albumArt = document.querySelector('.fullHero__artwork .image span');
+	const backgroundImage = albumArt.style.backgroundImage.split('"')[1];
+	const imageURL = backgroundImage.replace(/t500x500/g, 'original');
 
-  return imageURL;
+	return imageURL;
 }
 
 function getMixcloudImage() {
-  const albumArt = document.querySelector('.album-art img');
-  const lastImage = albumArt.srcset.split(',').pop();
-  const imageURL = lastImage.split(' ').shift().replace('900x900/', '');
+	const albumArt = document.querySelector('.album-art img');
+	const lastImage = albumArt.srcset.split(',').pop();
+	const imageURL = lastImage.split(' ').shift().replace('900x900/', '');
 
-  return imageURL;
+	return imageURL;
 }
 
 function getInstagramImage() {
-  const albumArt = document.querySelector('[style^="padding-bottom"] img');
-  const lastImage = albumArt.srcset.split(',').pop();
-  const imageURL = lastImage.split(' ').shift();
+	const albumArt = document.querySelector('[style^="padding-bottom"] img');
+	const lastImage = albumArt.srcset.split(',').pop();
+	const imageURL = lastImage.split(' ').shift();
 
-  return imageURL;
+	return imageURL;
 }
 
 function getYoutubeImage() {
-  let { groups } = window.location.href.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?(?<id>[^#&?]*)/);
+	const { groups } = window.location.href.match(
+		/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?(?<id>[^#&?]*)/,
+	);
 
-
-  return groups.id
-    ? `http://i3.ytimg.com/vi/${groups.id}/maxresdefault.jpg`
-    : null;
+	return groups.id ? `http://i3.ytimg.com/vi/${groups.id}/maxresdefault.jpg` : null;
 }
 
 function getBandcampImage() {
-  const albumArt = document.querySelector('#tralbumArt .popupImage img');
-  const imageURL = albumArt.src.replace('_16.jpg', '_0.jpg');
+	const albumArt = document.querySelector('#tralbumArt .popupImage img');
+	const imageURL = albumArt.src.replace('_16.jpg', '_0.jpg');
 
-  return imageURL;
+	return imageURL;
 }
 
 async function writeToClipboard(coverImage) {
-  try {
-    await navigator.clipboard.writeText(coverImage);
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		await navigator.clipboard.writeText(coverImage);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 (async () => {
-  window.focus();
+	window.focus();
 
-  const origin = window.location.origin.replace('//www.', '//');
-  let coverImage = '';
+	const origin = window.location.origin.replace('//www.', '//');
+	let coverImage = '';
 
-  switch (origin) {
-    case validOrigins.instagram:
-      coverImage = getInstagramImage()
-      break;
+	switch (origin) {
+		case validOrigins.instagram:
+			coverImage = getInstagramImage();
+			break;
 
-    case validOrigins.mixcloud:
-    case validOrigins.wwwMixcloud:
-      coverImage = getMixcloudImage()
-      break;
+		case validOrigins.mixcloud:
+		case validOrigins.wwwMixcloud:
+			coverImage = getMixcloudImage();
+			break;
 
-    case validOrigins.soundcloud:
-      coverImage = getSoundcloudImage()
-      break;
+		case validOrigins.soundcloud:
+			coverImage = getSoundcloudImage();
+			break;
 
-    case validOrigins.youtube:
-      coverImage = getYoutubeImage()
-      break;
+		case validOrigins.youtube:
+			coverImage = getYoutubeImage();
+			break;
 
-    default:
-      if (origin.endsWith('.bandcamp.com')) {
-        coverImage = getBandcampImage()
-        break;
-      }
+		default:
+			if (origin.endsWith('.bandcamp.com')) {
+				coverImage = getBandcampImage();
+				break;
+			}
 
-      window.alert(`This bookmarklet does not support ${origin}`);
-      return;
-  }
+			window.alert(`This bookmarklet does not support ${origin}`);
+			return;
+	}
 
-  if (!coverImage) return;
+	if (!coverImage) return;
 
-  await writeToClipboard(coverImage);
-  window.open(coverImage);
+	await writeToClipboard(coverImage);
+	window.open(coverImage);
 })();
